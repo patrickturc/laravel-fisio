94a365785039ef63a16989de69dc749858c3b72a
patrickturc <patrique_turchette@hotmail.com>
2026-06-14 17:05:47 -0300

    feat: add safety guard to TestCase to prevent non-sqlite database usage during testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,19 @@ use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        if (! $this->app) {
+            $this->refreshApplication();
+        }
+
+        if (config('database.default') !== 'sqlite') {
+            throw new \RuntimeException('DATABASE SAFETY GUARD: Running tests is blocked when the database connection is not "sqlite". Current connection is: ' . config('database.default'));
+        }
+
+        parent::setUp();
+    }
+
     protected function skipUnlessFortifyFeature(string $feature, ?string $message = null): void
     {
         if (! Features::enabled($feature)) {
